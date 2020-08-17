1e18eb5698f43b3e49883954b713a7a6ab6e7b80
mustafatok2016 <mustafa.tk33@gmail.com>
2020-08-17 00:37:43 +0000

    last version

---
+++ null.tf
@@ -0,0 +1,33 @@
+resource "null_resource"  "commands" {
+    depends_on = ["aws_instance.web"]
+    triggers = {
+        always_run = "${timestamp()}"
+    }
+    # Push files  to remote server
+    # provisioner "file" {
+    #     connection {
+    #     host = "${aws_instance.web.public_ip}"
+    #     type = "ssh"
+    #     user = "centos"
+    #     private_key = "${file("~/.ssh/id_rsa")}"
+    #     }
+    #     source      = "r1soft.repo"
+    #     destination = "/tmp/r1soft.repo"
+    # }
+    # Execute linux commands on remote machine
+    provisioner "remote-exec" {
+        connection {
+        host = "${aws_instance.web.public_ip}"
+        type = "ssh"
+        user = "centos"
+        private_key = "${file("~/.ssh/id_rsa")}"
+    }
+    inline = [
+        "sudo su -",
+        "sudo yum install curl -y ",
+        "sudo curl https://assets.nagios.com/downloads/nagiosxi/install.sh | sh",
+        
+        
+        ]
+    }
+}
